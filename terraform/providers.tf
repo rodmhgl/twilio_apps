432e7e0bd391054cc5f887d7af79c90c4879c0f5
Rod Stewart <rodstewart@gmail.com>
2023-07-14 12:42:39 -0500

    Adds terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">=1.5.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.28.1"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.63.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate6982"
+    container_name       = "tfstate"
+    key                  = "prod/twilio_do.tfstate"
+    use_oidc             = true
+  }
+
+}
+
+provider "azurerm" {
+  use_oidc = true
+  features {}
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+variable "do_token" {
+  type      = string
+  sensitive = true
+}
