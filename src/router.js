4426633bdea7b616c178a8d87800620c06af5165
Rod Stewart <rodstewart@gmail.com>
2023-08-01 23:34:52 -0500

    Updates router to ignore GET requests

--- src/router.js
+++ src/router.js
@@ -6,9 +6,9 @@ const callRouter = require('./call/router');
 const router = new Router();
 
 // GET: / - home page
-router.get('/', (req, res) => {
-  res.render('index');
-});
+// router.get('/', (req, res) => {
+//   res.render('index');
+// });
 
 router.use('/ivr', twilio.webhook({validate: false}), ivrRouter);
 router.use('/call', twilio.webhook({validate: false}), callRouter);
