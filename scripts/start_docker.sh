26d88e9ae0863288f9ca31a51e1a8956c5ce3cd3
Rod Stewart <rodstewart@gmail.com>
2025-02-10 10:26:25 -0600

    Externalize workflow shell scripts (#232)
    * Externalize workflow shell scripts
    
    Fixes #15
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/rodmhgl/twilio_apps/issues/15?shareId=XXXX-XXXX-XXXX-XXXX).
    
    * Change the shebang to use `/usr/bin/env bash` instead of `/bin/bash`

---
+++ scripts/start_docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eox pipefail
+
+docker pull rodstewart/twilio_apps:latest
+docker rm twilio_apps -f
+docker run --name twilio_apps -dit --restart unless-stopped -p 80:3000 rodstewart/twilio_apps:latest
